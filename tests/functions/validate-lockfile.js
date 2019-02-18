5d8926033da239e376134c1349c5332c6e2dcaea
chee <chee@snoot.club>
2019-02-18 05:37:25 +0000

    use the full name validateLockfile so it shows up in searches

--- tests/functions/validate-lockfile.js
+++ tests/functions/validate-lockfile.js
@@ -1,23 +1,23 @@
 import expect from "expect"
-import validate from "../../functions/validate-lockfile"
+import validateLockfile from "../../functions/validate-lockfile"
 
 describe("functions/validate-lockfile", () => {
 	it("accepts a version 1 lockfile with requires: true", () => {
-		return expect(validate({
+		return expect(validateLockfile({
 			lockfileVersion: 1,
 			requires: true
 		})).resolves.toBeUndefined()
 	})
 
 	it("does not accept a version 2 lockfile with requires: true", () => {
-		return expect(validate({
+		return expect(validateLockfile({
 			lockFileVersion: 2,
 			requires: true
 		})).rejects.toBeUndefined()
 	})
 
 	it("does not accept a version 1 lockfile without requires: true", () => {
-		return expect(validate({
+		return expect(validateLockfile({
 			lockFileVersion: 1
 		})).rejects.toBeUndefined()
 	})
