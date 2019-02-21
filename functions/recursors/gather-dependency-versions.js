e240c37c739c128f9b832dced4488178909c2c82
chee <chee@snoot.club>
2019-02-21 01:15:39 +0000

    take production flag in gather-dependency-versions

--- functions/recursors/gather-dependency-versions.js
+++ functions/recursors/gather-dependency-versions.js
@@ -3,12 +3,15 @@ import fastclone from "fast-clone"
 
 export let initial = {}
 
-export let createReducer = () =>
+export let createReducer = ({production = false} = {}) =>
 	function reducer (current, name, info) {
+		if (production && info.dev) {
+			return current
+		}
 		let counts = fastclone(current)
 		counts[name] = [...new Set((counts[name] || []).concat(info.version))]
 		return counts
 	}
 
-let recursor = () => createRecursor(createReducer(), initial)
+let recursor = ({production}) => createRecursor(createReducer({production}), initial)
 export default recursor
