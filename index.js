de42224ce58edc32594525f073d1a89b4cb2a47e
chee <chee@snoot.club>
2019-02-21 01:09:55 +0000

    demand that --file be a string

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let commandDir = path.resolve(__dirname, "commands")
 
 yargs
 	.option("file", {
-		describe: "the package-lock.json to operate on, use - or nothing for stdin"
+		describe: "the package-lock.json to operate on",
+		type: "string"
 	})
 	.commandDir(commandDir)
 	.demandCommand()
 	.help()
 	.epilogue("💖 🐕")
+	.showHelpOnFail(false)
 	.strict()
 	.argv
