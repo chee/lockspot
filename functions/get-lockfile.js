1ac96f04052c5673cb41c84ed5a3a608a7545f01
chee <chee@snoot.club>
2019-08-14 20:33:44 +0100

    yeet

--- functions/get-lockfile.js
+++
@@ -1,6 +0,0 @@
-import getInput from "./get-input"
-import parseLockfile from "./parse-lockfile"
-
-export default async function getLockfile ({file}) {
-	return parseLockfile(await getInput({file}))
-}
--- functions/validate-lockfile.js
+++
@@ -1,5 +0,0 @@
-export default async function validateLockfile (lockfile) {
-	return lockfile.lockfileVersion === 1 && lockfile.requires === true
-		? Promise.resolve()
-		: Promise.reject()
-}
